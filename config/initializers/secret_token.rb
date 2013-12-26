77b76baf71f33bbb2fb860d99f9229e7ddff9268
matthew <matthew.eichler@aventinesolutions.nl>
2013-12-26 19:34:33 +0100

    rails initialiseren

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DidactischeToolBelgie::Application.config.secret_key_base = 'db538736f5aa3c118aa7c267ab7313e88fb9a94b1eed08213b9a825755227f7d92327112b3f98fa348b4932544e2bcc736ec03aab84456629d34c984555ef464'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DidactischeToolBelgie::Application.config.session_store :cookie_store, key: '_didactische-tool-belgie_session'
